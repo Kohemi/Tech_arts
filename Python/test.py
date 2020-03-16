b5febe0edb843a44e3227662988741f48cec2714
kohemi <gostone11@gmail.com>
2020-03-16 14:53:14 +0900

    learn in afternoon 1. Python beginner course, installation, variables and memory

---
+++ Python/test.py
@@ -0,0 +1,13 @@
+# Practice 3.
+affiliation = "서울시립대학교"
+year = 2012
+print("나는", str(year)+"년도에",affiliation+"에 입학했습니다.")
+
+#Practice 4.
+x = float(input("enter x value : "))
+y = float(input("enter y value : "))
+print(x+y)
+
+#Practice 5.
+repetition = int(input("How many : "))
+print("*"*repetition)
