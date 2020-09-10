dfc51751822fab7ff9413cff10570d25081cbe7e
kohemi <gostone11@gmail.com>
2020-09-11 00:17:48 +0900

    Cpp practice First day

---
+++ Cpp/CppPractice/FuntionOverloading/FuntionOverloading.cpp
@@ -0,0 +1,112 @@
+// FuntionOverloading.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "CppPractice.h"
+
+using namespace std;
+
+//void Myfunc(void)
+//{
+//	cout << "Myfunc(void) called" << endl;
+//}
+//
+//void Myfunc(char c)
+//{
+//	cout << "Myfunc( char c ) called" << endl;
+//}
+//
+//void Myfunc(int a, int b)
+//{
+//	cout << "Myfunc(int a, int b) called" << endl;
+//
+//}
+//
+//int main(void)
+//{
+//	Myfunc();
+//	Myfunc('A');
+//	Myfunc(3, 4);
+//	return 0;
+//}
+
+//void swap(int* a, int* b)
+//{
+//	int temp = *a;
+//	*a = *b;
+//	*b = temp;
+//
+//}
+//
+//void swap(char* a, char* b)
+//{
+//	char temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//
+//void swap(double* a, double* b)
+//{
+//	double temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//
+//
+//int main(void)
+//{
+//	int num1 = 20, num2 = 30;
+//	swap(&num1, &num2);
+//	cout << num1 << ' ' << num2 << endl;
+//
+//	char ch1 = 'A', ch2 = 'B';
+//	swap(&ch1, &ch2);
+//	cout << ch1 << ' ' << ch2 << endl;
+//
+//	double db1 = 5.22, db2 = 6.212;
+//	swap(&db1, &db2);
+//	cout << db1 << ' ' << db2 << endl;
+//	return 0;
+//
+//}
+
+//int Adder(int num1 = 1, int num2 = 2)
+//{
+//	return num1 + num2;
+//}
+//
+//int main(void)
+//{
+//	cout << Adder() << endl;
+//	cout << Adder(5) << endl;
+//	cout << Adder(3, 5) << endl;
+//	return 0;
+//}
+
+//int BoxVolume(int length, int width = 1, int height = 1);
+//
+//int main(void)
+//{
+//	cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << endl;
+//	cout << "[5, 5, 5] : " << BoxVolume(5, 5) << endl;
+//	cout << "[5, D, D] : " << BoxVolume(5) << endl;
+//	//cout << "[D, D, D] : " << BoxVolume() << endl;
+//	return 0;
+//}
+//
+//int BoxVolume(int length, int width, int height)
+//{
+//	return length * width * height;
+//}
+
+//void BestComImpl::SimpleFunc(void)
+//{
+//	cout << "BestCom이 정의한 함수" << endl;
+//}
+//
+//void ProgComImpl::SimpleFunc(void)
+//{
+//	cout << "ProgCom이 정의한 함수" << endl;
+//}
+
+
